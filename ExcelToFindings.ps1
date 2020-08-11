048952f60dd177f91d1bdb1d90da5b1835c00716
morfaroth <morfaroth@hotmail.com>
2020-08-11 09:20:37 -0700

    Add files via upload
    Initial upload to Github

---
+++ ExcelToFindings.ps1
@@ -0,0 +1,64 @@
+﻿$objWord = New-Object -comobject Word.Application  
+$objWord.Visible = $false
+
+ 
+ 
+$MatchCase = $False 
+$MatchWholeWord = $true
+$MatchWildcards = $False 
+$MatchSoundsLike = $False 
+$MatchAllWordForms = $False 
+$Forward = $True 
+$Wrap = $wdFindContinue 
+$Format = $False 
+$wdReplaceNone = 0 
+$wdFindContinue = 1 
+$wdReplaceAll = 2
+$findingDocuments = @()
+
+Import-Csv .\findings.csv | ForEach-Object {
+    $objDoc = $objWord.Documents.Open("C:\Users\Matthew\Desktop\Template Find and Replace\FindingTemplate.docx") 
+    $objSelection = $objWord.Selection
+
+    $a = $objSelection.Find.Execute("<Name>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Name),$wdReplaceAll) 
+    
+    $a = $objSelection.Find.Execute("<Risk>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Risk),$wdReplaceAll)
+    
+    $a = $objSelection.Find.Execute("<Description>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Description),$wdReplaceAll)
+    
+    $a = $objSelection.Find.Execute("<Impact>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Impact),$wdReplaceAll) 
+
+    $a = $objSelection.Find.Execute("<AffectedResources>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.AffectedResources),$wdReplaceAll) 
+
+    $a = $objSelection.Find.Execute("<Recommendation>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Recommendation),$wdReplaceAll) 
+
+    $a = $objSelection.Find.Execute("<Scenario>",$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Scenario),$wdReplaceAll) 
+
+    $name = "C:\Users\Matthew\Desktop\Template Find and Replace\$($_.Name).docx"
+    $findingDocuments += "$($name)"
+    $objDoc.Saveas([ref]$name,[ref]$SaveFormat::wdFormatDocument)
+}
+
+$objDoc = $objWord.Documents.Add()
+$objSelection = $objWord.Selection
+foreach($finding in $findingDocuments){
+    $objSelection.TypeParagraph()
+    $objSelection.InsertFile($finding)
+}
+$name = "C:\Users\Matthew\Desktop\Template Find and Replace\allfindings.docx"
+$objDoc.Saveas([ref]$name,[ref]$SaveFormat::wdFormatDocument)
+$objWord.Quit()
---
+++ FindReplaceTemplate.ps1
@@ -0,0 +1,27 @@
+﻿$objWord = New-Object -comobject Word.Application  
+$objWord.Visible = $false
+
+$objDoc = $objWord.Documents.Open("C:\Users\Matthew\Desktop\Template Find and Replace\test.docx") 
+$objSelection = $objWord.Selection 
+ 
+$MatchCase = $False 
+$MatchWholeWord = $true
+$MatchWildcards = $False 
+$MatchSoundsLike = $False 
+$MatchAllWordForms = $False 
+$Forward = $True 
+$Wrap = $wdFindContinue 
+$Format = $False 
+$wdReplaceNone = 0 
+$wdFindContinue = 1 
+$wdReplaceAll = 2
+
+Import-Csv .\PentestReport-VARIABLES.csv | ForEach-Object {
+    Write-Host "$($_.VariableName)"
+    $a = $objSelection.Find.Execute($($_.VariableName),$MatchCase,$MatchWholeWord, ` 
+    $MatchWildcards,$MatchSoundsLike,$MatchAllWordForms,$Forward,` 
+    $Wrap,$Format,$($_.Value),$wdReplaceAll) 
+}
+
+$objDoc.Save()
+$objWord.Quit()
